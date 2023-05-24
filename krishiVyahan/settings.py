e1bf88743771b5c6cfb8467fc67d3c97703455db
Jaya Gupta <jayagupta752@gmail.com>
2023-05-24 23:44:40 +0530

    Update settings.py

--- krishiVyahan/settings.py
+++ krishiVyahan/settings.py
@@ -87,7 +87,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/krishi-vyahan',
+        default='postgresql://postgres:postgres@localhost:5432/krishiVyahan',
         conn_max_age=600
     )
 }
